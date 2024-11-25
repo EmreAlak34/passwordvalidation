47e94c827854f76083760241e1ab88ded276abf1
Emre Alak <alakemre@gmail.com>
2024-11-25 11:13:55 +0100

    Add gotDigitsMethod

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,4 +9,13 @@ public class Main {
     public static boolean minLength(String password) {
         return password.length() >= 8;  // Gibt true zurück, wenn die Länge des Passworts >= 8 ist
     }
+
+    public static boolean gotDigits(String password) {
+        for (int i = 0; i < password.length(); i++) {
+            if (password.charAt(i) >= '0' && password.charAt(i) <= '9') {
+                return true;
+            }
+        }
+        return false;
+    }
     }
